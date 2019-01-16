19f26d2237c63a26c4a3d3f6e4a152d45ae4bc11
John Andersen <john.s.andersen@intel.com>
2019-01-16 12:58:36 -0800

    test: fuzz: Added fuzzing TCTI
    * Created a test/fuzz/tcti/ directory which contains a TCTI to be used
      for fuzz testing.
    * Added ifdefs to sapi helpers and test-options in integration tests to
      enable static linking to a single TCTI.
    * Added configure.ac options to build fuzzing TCTI.
    * Added Makefile-fuzz.am used to build fuzzing TCTI.
    
    Signed-off-by: John Andersen <john.s.andersen@intel.com>

--- test/integration/sapi-context-util.c
+++ test/integration/sapi-context-util.c
@@ -12,10 +12,14 @@
 
 #include "tss2_tcti_device.h"
 #include "tss2_tcti_mssim.h"
+#ifdef HAVE_TCTI_FUZZING
+#include "tss2_tcti_fuzzing.h"
+#endif /* HAVE_TCTI_FUZZING */
 
 #include "context-util.h"
 #include "tss2-tcti/tcti-mssim.h"
 
+#ifdef HAVE_TCTI_DEVICE
 /*
  * Initialize a TSS2_TCTI_CONTEXT for the device TCTI.
  */
@@ -48,7 +52,9 @@ tcti_device_init(char const *device_path)
     }
     return tcti_ctx;
 }
+#endif /* HAVE_TCTI_DEVICE */
 
+#ifdef HAVE_TCTI_MSSIM
 /*
  * Initialize a socket TCTI instance using the provided options structure.
  * The hostname and port are the only configuration options used.
@@ -84,6 +90,43 @@ tcti_socket_init(char const *host, uint16_t port)
     }
     return tcti_ctx;
 }
+#endif /* HAVE_TCTI_MSSIM */
+
+#ifdef HAVE_TCTI_FUZZING
+/*
+ * Initialize a fuzzing TCTI instance using the provided options structure.
+ * The fuzzing_lengths.log file is the only configuration option used.
+ * The caller is returned a TCTI context structure that is allocated by this
+ * function. This structure must be freed by the caller.
+ */
+TSS2_TCTI_CONTEXT *
+tcti_fuzzing_init()
+{
+    size_t size;
+    TSS2_RC rc;
+    TSS2_TCTI_CONTEXT *tcti_ctx;
+
+    rc = Tss2_Tcti_Fuzzing_Init(NULL, &size, NULL);
+    if (rc != TSS2_RC_SUCCESS) {
+        fprintf(stderr, "Faled to get allocation size for tcti context: "
+                "0x%x\n", rc);
+        return NULL;
+    }
+    tcti_ctx = (TSS2_TCTI_CONTEXT *) calloc(1, size);
+    if (tcti_ctx == NULL) {
+        fprintf(stderr, "Allocation for tcti context failed: %s\n",
+                strerror(errno));
+        return NULL;
+    }
+    rc = Tss2_Tcti_Fuzzing_Init(tcti_ctx, &size, NULL);
+    if (rc != TSS2_RC_SUCCESS) {
+        fprintf(stderr, "Failed to initialize tcti context: 0x%x\n", rc);
+        free(tcti_ctx);
+        return NULL;
+    }
+    return tcti_ctx;
+}
+#endif /* HAVE_TCTI_FUZZING */
 
 /*
  * Initialize a SAPI context using the TCTI context provided by the caller.
@@ -147,10 +190,18 @@ TSS2_TCTI_CONTEXT *
 tcti_init_from_opts(test_opts_t * options)
 {
     switch (options->tcti_type) {
+#ifdef HAVE_TCTI_DEVICE
     case DEVICE_TCTI:
         return tcti_device_init(options->device_file);
+#endif /* HAVE_TCTI_DEVICE */
+#ifdef HAVE_TCTI_MSSIM
     case SOCKET_TCTI:
         return tcti_socket_init(options->socket_address, options->socket_port);
+#endif /* HAVE_TCTI_MSSIM */
+#ifdef HAVE_TCTI_FUZZING
+    case FUZZING_TCTI:
+        return tcti_fuzzing_init();
+#endif /* HAVE_TCTI_FUZZING */
     default:
         return NULL;
     }
